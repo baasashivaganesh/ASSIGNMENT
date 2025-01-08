d24728bc15fb4bea4a5279f510155805d05282ae
baasashivaganesh <shivaganesh0828@gmail.com>
2025-01-08 11:53:24 +0530

    data analytics

---
+++ functionEx.js
@@ -0,0 +1,17 @@
+//without parameters and arguements
+// function demo(){//function declaration
+//     console.log("i am a  function");
+// }
+// demo();
+// demo();
+// demo();//function calling
+
+//with parameters and arguements
+function add(a,b){
+    // console.log(a);
+    // console.log(b);
+    console.log(a+b);
+}
+add(5,6);
+add(7,7);
+add(9,0);
---
+++ loopingstatement.js
@@ -0,0 +1,60 @@
+// console.log("Hello world");
+
+// for(let i=1;i<=5;i++)
+// {
+//     console.log("hello world");
+// }
+// let n = 100;
+// for(let i=1;i<=n;i++){
+//     // console.log(i);
+//     if(i%2 == 0){
+//         console.log(i + " is a even number");
+//     }else{
+//         console.log(i + " is a odd number");
+//     }
+// }
+// function factorial(n) {
+//     if (n < 0) {
+//         return "Factorial is not defined for negative numbers.";
+//     }
+//     let result = 1;
+//     for (let i = 2; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+
+// const number = parseInt(prompt("Enter a number:"));
+// if (isNaN(number)) {
+//     console.log("Please enter a valid number.");
+// } else {
+//     console.log(`Factorial of ${number} is: ${factorial(number)}`);
+// }
+
+// function factorial(n){
+//     let result =1;
+// for(let i=2;i<=n;i++){
+//     result*=i;
+
+// }
+// return result;
+// }
+// console.log(factorial(5));
+
+let num = 5;
+let fact = 1;
+for(let i=1 ; i<=num;i++){
+    fact = fact*i;
+};
+console.log(fact);
+
+// !do-while loop
+// do{
+//     console.log("please makeup");
+//     i++;
+// }while(i >= 5);
+
+
+
+
