779490d7098f37dfd0d2c60c98c8424a4071acae
baasashivaganesh <shivaganesh0828@gmail.com>
2025-01-23 23:22:52 +0530

    hi

--- async.js
+++ async.js
@@ -23,31 +23,45 @@
 // }).finally(()=>console.log("finally printing the both"))
 
 
-// API fetching
-function fetchUsers()
-{
-    let x = fetch("https://jsonplaceholder.typicode.com/users");
-    console.log(x);
-    x
-    .then((response)=>{
-        //console.log(response)
-        //console.log(response.json());
-        return response.json().then(data=>{
-            console.log(data);
-            let store = document.getElementById("store");
-            data.map((user)=>{
-                store.innerHTML +=
-                <tr>
-                <td>${user.id}</td>
-                <td>${user.name}</td>
-                <td>${user.email}</td>
-                <td>${user.company.name}</td>
-                </tr>
-            })
-        })
-    })
-    .catch(err=>console.log(err))
+// function fetchUsers() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             let store = document.getElementById("store");
+//             data.forEach(user => {
+//                 store.innerHTML += `
+//                 <tr>
+//                     <td>${user.id}</td>
+//                     <td>${user.name}</td>
+//                     <td>${user.email}</td>
+//                     <td>${user.company.name}</td>
+//                 </tr>`;
+//             });
+//         })
+//         .catch(err => console.log(err));
+// }
 
-}
-fetchUsers();
+// fetchUsers();
+// let p = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         resolve("Promise successfully completing");
+//     } , 3000)
+// })
+// async function demo()
+// {
+//     console.log("start");
+//     let x =await(p);
+//     console.log(x);
+//     console.log("end");
+// }
+// demo();
 
+// async function fetchUsers(){
+//     let response =await fetch("https://api.github.com/users");
+//     let data =await response.json();
+//     console.log(data);
+// }
+// fetchUsers();
+
+console.log(document);
